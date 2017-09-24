d05fe443f35fdb922e4f329c962f57225923bf94
lufortes <lucyuyfortes@gmail.com>
2017-09-24 16:18:47 -0400

    Exercise 2.88
    Modify object to add a new private field, refNumber, with appropriate accessors, setRefNumber and getRefNumber.

--- Book.java
+++ Book.java
@@ -12,6 +12,7 @@ class Book
     private String author;
     private String title;
     private int pages;
+    private String refNumber;
 
     /**
      * Set the author and title fields when this object
@@ -22,6 +23,7 @@ class Book
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
+        refNumber = "";
     }
 
     /**
@@ -69,5 +71,19 @@ class Book
         System.out.println("Author: " + author);
         System.out.println("Pages:  " + pages);
         System.out.println("*******************************************");
+    }
+    /**
+     * Set reference number
+     */
+    public void setRefNumber(String ref)
+    {
+        refNumber = ref; 
+    }
+    /**
+     * Get reference number
+     */
+    public String getRefNumber()
+    {
+        return refNumber;
     }
 }
